f30cf98083577bf2a62364ad93b33648e13ce185
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-06-24 13:47:21 +0200

    684_Fixing a Small Bug

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -21,7 +21,7 @@ const UserInput = (props) => {
         setUserInput((prevInput) => {
             return {
                 ...prevInput,
-                [input]: value
+                [input]: +value
             }
         })
     }
